0536a3a9d5bdc39edf995a96b2cd5db64508e749
liubo731731 <1083428842@qq.com>
2019-03-30 21:49:43 +0800

    1

--- 代码部分/hello.php
+++ 代码部分/hello.php
@@ -1,11 +1,24 @@
-<html>
-<body>
-
-<form action="welcome.php" method="post">
-    Nam1e: <input type="text" name="fname">
-    Age: <input type="text" name="age">
-    <input type="submit">
-</form>
-
-</body>
-</html>
+<?php
+$q = isset($_GET['q'])? htmlspecialchars($_GET['q']) : '';
+if($q) {
+    if($q =='RUNOOB') {
+        echo 'http://www.runoob.com';
+    } else if($q =='GOOGLE') {
+        echo 'http://www.google.com';
+    } else if($q =='TAOBAO') {
+        echo 'http://www.taobao.com';
+    }
+} else {
+    ?>
+    <form action="" method="get">
+        <select name="q">
+            <option value="">选择一个站点:</option>
+            <option value="RUNOOB">Runoob</option>
+            <option value="GOOGLE">Google</option>
+            <option value="TAOBAO">Taobao</option>
+        </select>
+        <input type="submit" value="提交">
+    </form>
+    <?php
+}
+?>
