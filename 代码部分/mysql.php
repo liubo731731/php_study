2d21769ea39c9f00f8c45546f5bc42a09920d966
liubo731731 <1083428842@qq.com>
2019-04-04 20:20:41 +0800

    000

---
+++ 代码部分/mysql.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "192.198.10.76";
+$username = "root";
+$password = "admin";
+
+// 创建连接
+$conn = new mysqli($servername, $username, $password);
+
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+}
+echo "连接成功";
+?>
